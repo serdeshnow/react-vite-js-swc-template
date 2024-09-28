a1bee1958d19382dc0321b5beab590ac65ef8b8b
serdeshnow <vserdes1977@mail.ru>
2024-09-28 19:32:07 +0300

    feat: initial commit

---
+++ app/src/main.jsx
@@ -0,0 +1,15 @@
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import { App } from './App.jsx';
+import './styles/index.css';
+import './styles/class-names.scss';
+import './styles/variables.css';
+import './styles/normalize.css';
+
+const root = createRoot(document.getElementById('root'));
+
+root.render(
+	<StrictMode>
+		<App />
+	</StrictMode>,
+);
